67dab6dde75f3be8a423f49bade73c036c48efbd
Mohanasubi <mohanasubivenkatachalapathi>
2025-03-19 11:24:00 +0530

    UPDATE updated the code using oops structure

--- rectangle.js
+++ rectangle.js
@@ -1,14 +1,23 @@
-function area(length,breadth)
-{
-    return length*breadth
+class rectangle{
+    constructor(length,breadth){
+        this.length = length;
+        this.breadth = breadth
+    }
+
+    calculateArea(){
+        return this.length*this.breadth
+     }
+
+     calculatePerimeter(){
+        return 2*(this.length+this.breadth)
+     }
+
 }
 
-console.log(area(10,20))
+const rectangle4x5 = new rectangle(4,5);
+const rectangle10x20 = new rectangle(10,20);
 
-
-function calculatePerimeter(length,breadth)
-{
-    return 2*(length+breadth)
-}
-
-console.log(calculatePerimeter(10,20))
+console.log(`Area : ${rectangle4x5.calculateArea()}`)
+console.log(`Perimeter : ${rectangle4x5.calculatePerimeter()}`);
+console.log(`Area : ${rectangle10x20.calculateArea()}`)
+console.log(`Perimeter : ${rectangle10x20.calculatePerimeter()}`);
